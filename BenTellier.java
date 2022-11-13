26dc5ed5c62fa94d7655ade295884eedd2cf8d08
BenTellier4995 <bentellier8@gmail.com>
2022-11-13 19:33:15 +0200

    this is my first project update

--- BenTellier.java
+++ BenTellier.java
@@ -1,7 +1,7 @@
 public class BenTellier{
 public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Ben Tellier..."+i);
+			System.out.println(" My name is Ben Tellier..."+i);
 		}
 	}
 }
